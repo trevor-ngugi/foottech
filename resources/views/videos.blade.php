61ae3b65f85092d4f54ab532f6f67cc5c9bb2206
trevor ngugi <ngugitrevor@gmail.com>
2022-06-20 13:38:02 +0300

    add navbar properties

--- resources/views/videos.blade.php
+++ resources/views/videos.blade.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="en">
+
 <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
@@ -7,15 +8,53 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
+
 <body>
+    <nav class="navbar navbar-expand-lg navbar-light " style="background-color: #FF7D05EB;">
+        <a class="navbar-brand" href="#">Foot Tech</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto ">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                </li>
+            </ul>
+            <span class="navbar-text">
+            <ul class="navbar-nav "> 
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Team</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Scout</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Post</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Profile</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Message</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">LogOut</a>
+                </li>
+            </ul>
+            </span>
+
+        </div>
+    </nav>
     @foreach($data as $row)
     <div class="card" style="width: 18rem;">
         <video width="320" height="240" autoplay muted>
             <source src="{{asset('upload')}}/{{$row['video']}}" type="video/mp4">
         </video>
         <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <h5 class="card-title">like button,</h5>
+            <p class="card-text">caption</p>
             <a href="#" class="btn btn-primary">Go somewhere</a>
         </div>
     </div>

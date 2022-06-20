16f2cacc9dfc052ded20833de99b10da0c6b5927
trevor ngugi <ngugitrevor@gmail.com>
2022-06-20 12:29:54 +0300

    added card body and bootstrap

--- resources/views/videos.blade.php
+++ resources/views/videos.blade.php
@@ -1,5 +1,25 @@
-@foreach($data as $row)
-<video width="320" height="240" autoplay muted>
-    <source src="{{asset('upload')}}/{{$row['video']}}" type="video/mp4">
-</video>
-@endforeach
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body>
+    @foreach($data as $row)
+    <div class="card" style="width: 18rem;">
+        <video width="320" height="240" autoplay muted>
+            <source src="{{asset('upload')}}/{{$row['video']}}" type="video/mp4">
+        </video>
+        <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+    </div>
+    @endforeach
+</body>
+
+</html>

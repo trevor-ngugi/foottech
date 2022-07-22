63cc29cd7fac38dedcba51271ca69971bafe7172
trevor ngugi <ngugitrevor@gmail.com>
2022-07-22 13:08:48 +0300

    add like button

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,8 +14,10 @@
     <script src="{{ asset('js/app.js') }}" defer></script>
 
     <!-- Fonts -->
+    <script src="https://kit.fontawesome.com/db5e67eceb.js" crossorigin="anonymous"></script>
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/db5e67eceb.js" crossorigin="anonymous"></script>
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
--- resources/views/videos.blade.php
+++ resources/views/videos.blade.php
@@ -6,6 +6,7 @@
 
 @foreach($data as $row)
 <div class="card" style="width: 21rem;">
+    <p class="card-text">userpic username</p>
     <video id="myvideo" width="320" height="240" controls>
         <source src="{{asset('upload')}}/{{$row['video']}}" type="video/mp4">
     </video>
@@ -17,9 +18,8 @@
         }
     </script>
     <div class="card-body">
-        <h5 class="card-title">like button,</h5>
-        <p class="card-text">caption</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
+        <button class="btn"><i class="fa-brands fa-gratipay"></i></button>
+        <p class="card-text">user caption</p>
     </div>
 </div>
 @endforeach
